24475a5711b59e8990ee76daea768e53c7720c28
joao <joaovlima07@hotmail.com>
2021-06-04 15:32:43 -0300

    adding a cart reducer

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -1,20 +1,45 @@
+import { useReducer } from 'react'
+
 import CartContext from './Cart-context'
 
-const CartProvider = (props) => {
-    const addItemToCarHandler = item => {}
-    
-    const removeItemFromCarHandler = id => {}
+const defaultCartState = { items: [], totalAmount: 0 }
 
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD':
+            const updatedItems = state.items.concat(action.item)
+            const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+            return {
+                items: updatedItems,
+                totalAmount: updatedTotalAmount
+            }
+            default:
+            return defaultCartState
+    }
+}
+
+const CartProvider = (props) => {
+
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+
+    const addItemToCarHandler = (item) => {
+        dispatchCartAction({ type: 'ADD', item: item })
+    }
+
+    const removeItemFromCarHandler = (id) => {
+        dispatchCartAction({ type: 'REMOVE', id: id })
+
+    }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCarHandler,
         removeItem: removeItemFromCarHandler
     }
 
     return (
-        <CartContext.Provider values={cartContext}>
+        <CartContext.Provider value={cartContext}>
             {props.children}
         </CartContext.Provider>
     )
--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,7 +1,17 @@
 import classes from './HeaderCartButton.module.css'
 import CartIcon from '../../Cart/CartIcon'
 
+import {useContext} from 'react'
+import CartContext from '../../../Store/Cart-context'
+
 const HeaderCartButton = (props) =>{
+
+    const cartCtx = useContext(CartContext)
+
+    const numberOfCartItems = cartCtx.items.reduce((acc, item) =>{
+        return acc + item
+    }, 0)
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
@@ -11,7 +21,7 @@ const HeaderCartButton = (props) =>{
             <span>Your Cart</span>
 
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
