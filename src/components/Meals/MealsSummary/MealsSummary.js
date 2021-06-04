f69b88e8f8577e3246be38d1f4b0304c2b1a7bb3
joao <joaovlima07@hotmail.com>
2021-06-04 10:33:27 -0300

    my first commit

---
+++ src/components/Meals/MealsSummary/MealsSummary.js
@@ -0,0 +1,11 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = ()=>{
+    return (
+        <section className>
+            <h2>Delicious Food, Delivered to You</h2>
+        </section>
+    )
+}
+
+export default MealsSummary
