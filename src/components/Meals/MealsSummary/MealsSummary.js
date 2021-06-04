5f24b847c3694d4d15011685f5b2fcd6550da00a
joao <joaovlima07@hotmail.com>
2021-06-04 10:46:08 -0300

    adding meals components

--- src/components/Meals/MealsSummary/MealsSummary.js
+++ src/components/Meals/MealsSummary/MealsSummary.js
@@ -1,9 +1,20 @@
 import classes from './MealsSummary.module.css'
 
-const MealsSummary = ()=>{
+const MealsSummary = () => {
     return (
-        <section className>
+        <section className={classes.summary}>
             <h2>Delicious Food, Delivered to You</h2>
+
+            <p>
+                Choose your favorite meal from our broad selection of available meals
+                and enjoy a delicious lunch or dinner at home.
+            </p>
+
+            <p>
+                All our meals are cooked with high-quality ingredients, just-in-time and
+                of course by experienced chefs!
+            </p>
+
         </section>
     )
 }
